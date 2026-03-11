e0c2634a9f9cd1d51d763d4b046c92f21503b3e3
liuyouhao <liuyouhao@7ugjoy.com>
2026-03-11 13:33:07 +0800

    changesh

--- hs.sh
+++ hs.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 port=18002
 pw=haolie
-sudo wget –no-check-certificate -O app.js https://raw.githubusercontent.com/haolie/yimer/master/showInstall.js
+sudo wget –no-check-certificate -O showInstall.sh https://raw.githubusercontent.com/haolie/yimer/master/showInstall.sh
+sudo chmod +x showInstall.sh
 sudo ./showInstall.sh haolie $pw $port 2>&1 | tee shadowsocks.log
 sed -i 's/collections.MutableMapping/collections.abc.MutableMapping/g' /usr/local/lib/python3.12/site-packages/shadowsocks-3.0.0-py3.12.egg/shadowsocks/lru_cache.py
 sudo firewall-cmd --permanent --add-port=$port/tcp 
