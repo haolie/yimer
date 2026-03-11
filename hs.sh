f4bec9470ccaf2744d74977229624f1f28514480
liuyouhao <liuyouhao@7ugjoy.com>
2026-03-11 14:12:16 +0800

    changesh

--- hs.sh
+++ hs.sh
@@ -4,7 +4,7 @@ pw=haolie
 sudo wget –no-check-certificate -O showInstall.sh https://raw.githubusercontent.com/haolie/yimer/master/showInstall.sh
 sudo chmod +x showInstall.sh
 sudo ./showInstall.sh haolie $pw $port 2>&1 | tee shadowsocks.log
-sed -i 's/collections.MutableMapping/collections.abc.MutableMapping/g' /usr/local/lib/python3.12/site-packages/shadowsocks-3.0.0-py3.12.egg/shadowsocks/lru_cache.py
+sudo sed -i 's/collections.MutableMapping/collections.abc.MutableMapping/g' /usr/local/lib/python3.12/site-packages/shadowsocks-3.0.0-py3.12.egg/shadowsocks/lru_cache.py
 sudo firewall-cmd --permanent --add-port=$port/tcp 
 sudo firewall-cmd --reload
 cd /etc/init.d/
