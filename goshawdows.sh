b31198b34bb58d32430e921a4bec83131476741e
haolie <haolie2006@163.com>
2026-07-21 16:01:26 +0800

    eidteport

--- goshawdows.sh
+++ goshawdows.sh
@@ -3,6 +3,6 @@ sudo mkdir /home/temp
 sudo chmod 777 /home/temp
 sudo wget –no-check-certificate -O /home/temp/shadows https://raw.githubusercontent.com/haolie/yimer/master/shadows
 sudo chmod +x /home/temp/shadows
-sudo firewall-cmd --permanent --add-port=18006/tcp 
+sudo firewall-cmd --permanent --add-port=18010/tcp 
 sudo firewall-cmd --reload
-sudo /home/temp/shadows -s 'ss://AES-256-GCM:haolie@:18006' -verbose
+sudo /home/temp/shadows -s 'ss://AES-256-GCM:haolie@:18010' -verbose
